e76f281db74cc2710ca5aae2711f6db4b6675fb3
PicciJr <piccijunior2319@gmail.com>
2021-10-04 08:17:59 +0200

    format numbers to avoid large decimals

--- src/calculator/calculator.service.ts
+++ src/calculator/calculator.service.ts
@@ -5,21 +5,26 @@ import { IProduct } from 'src/product/types'
 
 @Injectable()
 export class CalculatorService {
-
   createCalculator(item: IProduct, quantity: number, tariff: ShippingTariff) {
     const subtotal = item.price * quantity
     return {
-      subtotal,
+      subtotal: this.roundDownTwoDecimals(subtotal),
       shippingCosts: tariff,
-      total: subtotal + tariff,
+      total: this.roundDownTwoDecimals(subtotal + tariff),
     }
   }
 
   recalculateTotals(cart: CartDocument) {
-    const subtotal = cart.items.reduce((accumulator, product) => {
-      return accumulator + product.price * product.amount
-    }, 0)
-    const total = subtotal + cart.shippingCosts
+    const subtotal = this.roundDownTwoDecimals(
+      cart.items.reduce((accumulator, product) => {
+        return accumulator + product.price * product.amount
+      }, 0)
+    )
+    const total = this.roundDownTwoDecimals(subtotal + cart.shippingCosts)
     return { subtotal, shippingCosts: cart.shippingCosts, total }
+  }
+
+  roundDownTwoDecimals(number: number) {
+    return Math.round(number * 100) / 100
   }
 }
